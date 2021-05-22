8b4d2f9a8d39db02048b65dea76bb7b8aeb3144e
root <root@edge-14.edge.lab.eng.rdu2.redhat.com>
2021-05-22 04:08:55 -0400

    Add VM_EXTRADISKS_SIZE config option

--- config_example.sh
+++ config_example.sh
@@ -204,6 +204,9 @@ set -x
 # names delimited by spaces. Example "vdb vdc"
 # export VM_EXTRADISKS_LIST="vdb vdc"
 
+# Configure size of extra disks added to VMs
+# export VM_EXTRADISKS_SIZE="10G"
+
 # Provide additional master/worker ignition configuration, will be
 # merged with the installer provided config, can be used to modify
 # the default nic configuration etc
